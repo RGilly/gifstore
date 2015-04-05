4bbb14f6dbde9afd125766951abcf82d205f937d
Ryan Gilman <rygilman@gmail.com>
2015-04-05 14:40:07 -0400

    validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,8 @@ class Image < ActiveRecord::Base
 	mount_uploader :file, ImageUploader
 	belongs_to :user
 
-	#validates :file, presence: true
+	validates :file, presence: true
+	validates :title, presence: true
+	validates :categories, presence: true
 
 end
